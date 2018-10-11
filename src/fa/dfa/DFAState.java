41b2cb197e9ce14f8ecd583fd4ba8c0f87906cc9
Nathan Regner <nathanregner@gmail.com>
2018-10-10 18:20:12 -0600

    Update JavaDocs + formatting

--- src/fa/dfa/DFAState.java
+++ src/fa/dfa/DFAState.java
@@ -6,62 +6,63 @@ import java.util.HashMap;
 import java.util.Objects;
 import java.util.Set;
 
-/*
- *  Starting instantiating a few simple things, state storage (Hashmap) State constructor, and transitions
- * 
+/**
+ * Starting instantiating a few simple things, state storage (Hashmap) State constructor, and transitions
  */
-public class DFAState extends State{
+public class DFAState extends State {
 
-/*
- * Instantiating the Hashmap that will represent the states
- */
-	private HashMap<Character, DFAState> transition;
-/*
- * State constructor
- * @param name - Name for the state
- */
+    /**
+     * Instantiating the Hashmap that will represent the states
+     */
+    private HashMap<Character, DFAState> transition;
 
-	public DFAState(String name) {
-		transition = new HashMap<Character, DFAState>();
-		this.name = name;
-	
-	}
-/*
- * transitions constructor
- * @param toState - state name for transition
- * @parma c - consumed input
- */
-	public void addATransition(DFAState toState, char c) {
-		transition.put(c, toState);
+    /**
+     * State constructor
+     *
+     * @param name Name for the state
+     */
+    public DFAState(String name) {
+        transition = new HashMap<>();
+        this.name = name;
+    }
 
-	
-	}
-/*
- * transitions possible or not
- * @param c - char to be consumed if possible
- * @returns - the statename that you can go to. 
- */
-	public DFAState getTransition(char c) {
-	if(transition.containsKey(c))
-		return transition.get(c);
-	else
-		return null;
-	}
+    /**
+     * Adds a transition to the given state
+     *
+     * @param toState state name for transition
+     * @param c       consumed input
+     */
+    public void addATransition(DFAState toState, char c) {
+        transition.put(c, toState);
+    }
 
-	public Set<Character> getTransitions() {
-		return transition.keySet();
-	}
+    /**
+     * Checks if a givne transition is
+     *
+     * @param c char to be consumed if possible
+     * @return the state that you can go to, or null
+     */
+    public DFAState getTransition(char c) {
+        return transition.get(c);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		DFAState dfaState = (DFAState) o;
-		return Objects.equals(name, dfaState.name);
-	}
+    /**
+     * @return the set of all transition characters
+     */
+    public Set<Character> getTransitions() {
+        return transition.keySet();
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(name);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DFAState dfaState = (DFAState) o;
+        return Objects.equals(name, dfaState.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
